99d4effba33ec4bd11d9c330e3759a148b7a8842
ramesh67giri <ramesh67giri@gmail.com>
2026-05-21 09:04:38 +0900

    deploy

--- Project-1/script.js
+++ Project-1/script.js
@@ -7,3 +7,19 @@ document.getElementById('task-list');
 const inputText = document.getElementById('input-text');
 const addButtion = document.getElementById('js-add-me-button');
 const taskList = document.getElementById('task-list');
+
+// now i am adding in function
+addButtion.addEventListener('click', function () {
+  // get input value
+  const task = inputText.value
+  // we create a list using javascript in HTML 
+  const li = document.createElement('li');
+  // adding text
+  li.textContent = task;
+
+  // show on page 
+  taskList.appendChild(li);
+  // clear input box 
+  inputText.value = '';
+})
+
